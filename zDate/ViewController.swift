c45af755b96dfb723554151d6c51a901f9be713e
zac1st1k <zacfirst@gmail.com>
2015-04-03 23:07:39 +1100

    Initial Commit

---
+++ zDate/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  zDate
+//
+//  Created by Zac on 3/04/2015.
+//  Copyright (c) 2015 1st1k. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
