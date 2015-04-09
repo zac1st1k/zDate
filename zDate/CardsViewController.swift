40fddc20c3f7bfa2d901071bd35dd3a8cf62b405
zac1st1k <zacfirst@gmail.com>
2015-04-09 22:43:10 +1000

    + Refactor with Card Struct

--- zDate/CardsViewController.swift
+++ zDate/CardsViewController.swift
@@ -10,24 +10,27 @@ import UIKit
 
 class CardsViewController: UIViewController, SwipeViewDelegate {
     
+    struct Card {
+        let cardView: CardView
+        let swipeView: SwipeView
+    }
+    
     let frontCardTopMargin: CGFloat = 0
     let backCardTopMargin: CGFloat = 10
     @IBOutlet weak var cardStackView: UIView!
     
-    var backCard: SwipeView?
-    var frontCard: SwipeView?
+    var backCard: Card?
+    var frontCard: Card?
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         cardStackView.backgroundColor = UIColor.clearColor()
-        backCard = SwipeView(frame: createCardFrame(backCardTopMargin))
-        backCard?.delegate = self
-        cardStackView.addSubview(backCard!)
-        frontCard = SwipeView(frame: createCardFrame(frontCardTopMargin))
-        frontCard?.delegate = self
-        cardStackView.addSubview(frontCard!)
+        backCard = createCard(backCardTopMargin)
+        cardStackView.addSubview(backCard!.swipeView)
+        frontCard = createCard(frontCardTopMargin)
+        cardStackView.addSubview(frontCard!.swipeView)
     }
 
     override func didReceiveMemoryWarning() {
@@ -50,19 +53,27 @@ class CardsViewController: UIViewController, SwipeViewDelegate {
         return CGRect(x: 0, y: topMargin, width: cardStackView.frame.width, height: cardStackView.frame.height)
     }
     
+    private func createCard(topMargin: CGFloat) -> Card {
+        let cardView = CardView()
+        let swipeView = SwipeView(frame: createCardFrame(topMargin))
+        swipeView.delegate = self
+        swipeView.innerView = cardView
+        return Card(cardView: cardView, swipeView: swipeView)
+    }
+    
     // MARK: - SwipeDelegate
     
     func swipedLeft() {
         println("left")
         if let frontCard = frontCard {
-            frontCard.removeFromSuperview()
+            frontCard.swipeView.removeFromSuperview()
         }
     }
     
     func swipedRight() {
         println("right")
         if let frontCard = frontCard {
-            frontCard.removeFromSuperview()
+            frontCard.swipeView.removeFromSuperview()
         }
     }
 }
