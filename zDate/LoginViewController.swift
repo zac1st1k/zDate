1061019a73c74615714a5e607e8544df24ebe1ab
zac1st1k <zacfirst@gmail.com>
2015-04-12 23:52:46 +1000

    + Saving User to Parse

--- zDate/LoginViewController.swift
+++ zDate/LoginViewController.swift
@@ -40,6 +40,22 @@ class LoginViewController: UIViewController {
             }
             else if user.isNew {
                 println("User signed up and logged in through Facebook!")
+                FBRequestConnection.startWithGraphPath("/me?fields=picture,first_name,birthday,gender", completionHandler: {
+                    connection, result, error in
+                    var r = result as NSDictionary
+                    user["firstName"] = r["first_name"]
+                    user["gender"] = r["gender"]
+                    user["picture"] = ((r["picture"] as NSDictionary)["data"] as NSDictionary) ["url"]
+                    var dateFormatter = NSDateFormatter()
+                    dateFormatter.dateFormat = "MM/dd/yyyy"
+                    user["birthday"] = dateFormatter.dateFromString(r["birthday"] as String)
+                    user.saveInBackgroundWithBlock({
+                        success, error in
+                        println(success)
+                        println(error)
+                    })
+                    }
+                )
             }
             else {
                 println("User logged in through Facebook!")
