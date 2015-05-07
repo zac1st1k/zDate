88d9251d7aa37381a361ec09447fec27c8672346
zac1st1k <zacfirst@gmail.com>
2015-05-07 23:32:48 +1000

    + UserCell

---
+++ zDate/UserCell.swift
@@ -0,0 +1,24 @@
+//
+//  UserCell.swift
+//  zDate
+//
+//  Created by Zac on 7/05/2015.
+//  Copyright (c) 2015 1st1k. All rights reserved.
+//
+
+import UIKit
+
+class UserCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
